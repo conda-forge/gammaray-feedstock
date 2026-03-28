32663c598869eee4a4ecbc28ccdbf621c85cc67b
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-28 01:45:02 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cmake -S ${SRC_DIR} -B build -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ${CMAKE_ARGS}
+cmake -S ${SRC_DIR} -B build -G Ninja -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ${CMAKE_ARGS}
 cmake --build build -j ${CPU_COUNT}
 cmake --install build
